ee983a8efb084362309f929914409884cd9dd52f
Gleb Ivashyn <glebivashyn@gmail.com>
2024-05-25 00:11:24 +0300

    simple connection test

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
-# This is a sample Python script.
+import requests
+from time import sleep
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+def check_url():
+    url = 'https://www.autoscout24.com/lst?atype=C&body=3&cy=D%2CA%2CB%2CE%2CF%2CI%2CL%2CNL&damaged_listing=exclude&desc=1&doorfrom=2&doorto=3&fregfrom=2000&fregto=2014&fuel=B&gear=M&powerfrom=132&powertype=hp&priceto=12500&search_id=d8i3umblu0&sort=age&source=detailsearch&ustate=N%2CU'
 
+    response = requests.get(url)
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    if response.status_code == 200:
+        print("Successfully connected to the website. Status code:", response.status_code)
+        print(response.text)
+    else:
+        print("Failed to connect. Status code:", response.status_code)
 
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+if __name__ == "__main__":
+    check_url()
